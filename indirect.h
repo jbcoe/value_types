9ef63e63944021acb1c04ab4a66413580f875581
Jonathan B. Coe <jonathanbcoe@gmail.com>
2023-09-22 21:11:42 +0100

    Add missing header for assert

--- indirect.h
+++ indirect.h
@@ -19,6 +19,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #ifndef XYZ_INDIRECT_H
 #define XYZ_INDIRECT_H
+
+#include <cassert>
 #include <memory>
 #include <utility>
 
--- polymorphic.h
+++ polymorphic.h
@@ -21,6 +21,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #ifndef XYZ_POLYMORPHIC_H_
 #define XYZ_POLYMORPHIC_H_
 
+#include <cassert>
 #include <memory>
 #include <utility>
 
