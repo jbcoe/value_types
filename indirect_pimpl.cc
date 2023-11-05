92e75e1e51db53e5eb63edc27ff6f79aae656551
Jonathan B. Coe <jonathanbcoe@gmail.com>
2023-11-05 15:52:22 +0000

    Add method use tests for PIMPL and incomplete types

--- indirect_pimpl.cc
+++ indirect_pimpl.cc
@@ -14,3 +14,12 @@ Class& Class::operator=(const Class&) = default;
 
 void Class::do_something() { impl_->do_something(); }
 }  //  namespace xyz::testing
+
+void check_methods() {
+  xyz::testing::Class c; // Default construction.
+  auto cc = c; // Copy construction.
+  auto mc = std::move(c); // Move construction.
+  c = cc; // Copy assignment.
+  c = xyz::testing::Class(); // Move assignment.
+  c.do_something();
+}
--- polymorphic_pimpl.cc
+++ polymorphic_pimpl.cc
@@ -20,3 +20,12 @@ ClassWithPolymorphicPimpl& ClassWithPolymorphicPimpl::operator=(
 
 void ClassWithPolymorphicPimpl::do_something() { impl_->do_something(); }
 }  //  namespace xyz::testing
+
+void check_methods() {
+  xyz::testing::ClassWithPolymorphicPimpl c; // Default construction.
+  auto cc = c; // Copy construction.
+  auto mc = std::move(c); // Move construction.
+  c = cc; // Copy assignment.
+  c = xyz::testing::ClassWithPolymorphicPimpl(); // Move assignment.
+  c.do_something();
+}
