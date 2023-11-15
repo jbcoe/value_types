500e86678575168862fb3edc2d995e05a5ecb49a
Jonathan B. Coe <jonathanbcoe@gmail.com>
2023-11-15 22:45:57 +0000

    Fix missing licenses, headers, sort constructor initializers

--- compatibility/indirect_cxx11.h
+++ compatibility/indirect_cxx11.h
@@ -1,3 +1,23 @@
+/* Copyright (c) 2016 The Value Types Authors. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+==============================================================================*/
+
 #ifndef XYZ_INDIRECT_H
 #define XYZ_INDIRECT_H
 
@@ -104,13 +124,13 @@ class indirect : private detail::empty_base_optimization<A> {
   }
 
   indirect(indirect&& other) noexcept
-      : p_(nullptr), alloc_base(other.alloc_base::get()) {
+      : alloc_base(other.alloc_base::get()), p_(nullptr) {
     assert(other.p_ != nullptr);  // LCOV_EXCL_LINE
     std::swap(p_, other.p_);
   }
 
   indirect(std::allocator_arg_t, const A& alloc, indirect&& other) noexcept
-      : p_(nullptr), alloc_base(alloc) {
+      : alloc_base(alloc), p_(nullptr) {
     assert(other.p_ != nullptr);  // LCOV_EXCL_LINE
     std::swap(p_, other.p_);
   }
