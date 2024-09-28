92187b511fe3d5feb6f1f61381747df2a243c8ff
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-09-28 08:22:16 -0700

    Add build scripts for ease of use (#435)

---
+++ scripts/cmake.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux -o pipefail
+
+# Make a build directory
+mkdir -p build
+
+# Generate build system specified in build directory with cmake
+cmake -Bbuild
+
+# Build the underlying build system via CMake
+cmake --build build
+
+# Run the tests
+ctest --test-dir build
