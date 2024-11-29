810900468ee69e591321cd78249c7359a3f54b0c
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-11-29 21:49:01 +0000

    Cleanup

--- compile_checks/indirect_as_member.cc
+++ compile_checks/indirect_as_member.cc
@@ -1,16 +1,22 @@
-#include "indirect.h"
+#include <concepts>
+#include <type_traits>
 
+#include "indirect.h"
 namespace xyz::testing {
 
 struct number {};
 
 struct Composite {
   xyz::indirect<struct A> data;
+  friend bool operator==(const Composite&, const Composite&) = default;
 };
 
-struct A {};
+struct A {
+  friend bool operator==(const A&, const A&) = default;
+};
 
-static_assert(std::is_copy_constructible_v<Composite>);
 static_assert(std::is_default_constructible_v<Composite>);
+static_assert(std::is_copy_constructible_v<Composite>);
+static_assert(std::equality_comparable<Composite>);
 
 }  //  namespace xyz::testing
--- compile_checks/indirect_in_variant.cc
+++ compile_checks/indirect_in_variant.cc
@@ -1,18 +1,29 @@
+#include <concepts>
+#include <type_traits>
 #include <variant>
 
 #include "indirect.h"
+
 namespace xyz::testing {
 
-struct Number {};
+struct Number {
+  friend bool operator==(const Number&, const Number&) = default;
+};
+
 struct BinOp;
 
 struct Expression {
-  std::variant<Number, xyz::indirect<BinOp>> info;
+  std::variant<xyz::indirect<BinOp>, Number> info;
+
+  friend bool operator==(const Expression&, const Expression&) = default;
 };
 
-struct BinOp {};
+struct BinOp {
+  friend bool operator==(const BinOp&, const BinOp&) = default;
+};
 
 static_assert(std::is_default_constructible_v<Expression>);
 static_assert(std::is_copy_constructible_v<Expression>);
+static_assert(std::equality_comparable<Expression>);
 
 }  //  namespace xyz::testing
