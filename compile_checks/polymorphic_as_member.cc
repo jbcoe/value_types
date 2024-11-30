9942a0b7ba60e3512fd03a882581bf3f36191531
Jonathan B. Coe <jonathanbcoe@gmail.com>
2024-11-30 13:18:53 +0000

    Add missing files

---
+++ compile_checks/polymorphic_as_member.cc
@@ -0,0 +1,32 @@
+#include <concepts>
+#include <type_traits>
+
+#include "polymorphic.h"
+namespace xyz::testing {
+
+struct A;
+
+struct Composite {
+  xyz::polymorphic<A> data;
+
+  Composite();
+  Composite(const Composite&);
+  Composite(Composite&&);
+  Composite& operator=(const Composite&);
+  Composite& operator=(Composite&&);
+
+  friend bool operator==(const Composite&, const Composite&);
+  friend std::strong_ordering operator<=>(const Composite&, const Composite&);
+};
+
+struct A {
+  friend bool operator==(const A&, const A&);
+  friend std::strong_ordering operator<=>(const A&, const A&);
+};
+
+static_assert(std::is_default_constructible_v<Composite>);
+static_assert(std::is_copy_constructible_v<Composite>);
+static_assert(std::equality_comparable<Composite>);
+static_assert(std::three_way_comparable<Composite>);
+
+}  //  namespace xyz::testing
---
+++ compile_checks/polymorphic_in_variant.cc
@@ -0,0 +1,39 @@
+#include <concepts>
+#include <type_traits>
+#include <variant>
+
+#include "polymorphic.h"
+
+namespace xyz::testing {
+
+struct Number {
+  friend bool operator==(const Number&, const Number&);
+  friend std::strong_ordering operator<=>(const Number&, const Number&);
+};
+
+struct BinOp;
+
+struct Expression {
+  std::variant<Number, xyz::polymorphic<BinOp>> info;
+
+  Expression();
+  Expression(const Expression&);
+  Expression(Expression&&);
+  Expression& operator=(const Expression&);
+  Expression& operator=(Expression&&);
+
+  friend bool operator==(const Expression&, const Expression&);
+  friend std::strong_ordering operator<=>(const Expression&, const Expression&);
+};
+
+struct BinOp {
+  friend bool operator==(const BinOp&, const BinOp&);
+  friend std::strong_ordering operator<=>(const BinOp&, const BinOp&);
+};
+
+static_assert(std::is_default_constructible_v<Expression>);
+static_assert(std::is_copy_constructible_v<Expression>);
+static_assert(std::equality_comparable<Expression>);
+static_assert(std::three_way_comparable<Expression>);
+
+}  //  namespace xyz::testing
