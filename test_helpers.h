c9bdf981d1c092628b5ab3e550dd541e9bba8d73
Jonathan B. Coe <jonathanbcoe@gmail.com>
2025-01-12 16:31:23 +0000

    Update impl and tests to remove deferred constraints

--- test_helpers.h
+++ test_helpers.h
@@ -29,6 +29,14 @@ constexpr bool static_test() {
   return B;
 }
 
+struct NonCopyable {
+  NonCopyable() = default;
+  NonCopyable(const NonCopyable&) = delete;
+  NonCopyable& operator=(const NonCopyable&) = delete;
+  NonCopyable(NonCopyable&&) = default;
+  NonCopyable& operator=(NonCopyable&&) = default;
+};
+
 }  // namespace xyz
 
 #endif  // XYZ_TEST_HELPERS_H
