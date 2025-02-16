4bf19174e219c6a252d6856f3be2bbd433137b1d
Jonathan B. Coe <jonathanbcoe@gmail.com>
2025-02-16 19:05:02 +0100

    Add deduction guides for polymorphic

--- polymorphic_test.cc
+++ polymorphic_test.cc
@@ -845,4 +845,44 @@ TEST(PolymorphicTest, TaggedAllocatorsNotEqualMoveConstructFromValueless) {
   EXPECT_TRUE(iii.valueless_after_move());
 }
 
+TEST(PolymorphicTest, TemplateArgumentDeduction) {
+  xyz::polymorphic p(Derived(4));
+
+  EXPECT_EQ(p->value(), 4);
+}
+
+TEST(PolymorphicTest, TemplateArgumentDeductionCopy) {
+  xyz::polymorphic p(Derived(4));
+  xyz::polymorphic pp(p);
+
+  static_assert(std::is_same_v<decltype(p), decltype(pp)>);
+
+  EXPECT_EQ(pp->value(), 4);
+}
+
+TEST(PolymorphicTest, TemplateArgumentDeductionWithAllocator) {
+  xyz::TaggedAllocator<int> a(1);
+  xyz::polymorphic p(std::allocator_arg, a, Derived(4));
+
+  EXPECT_EQ(p->value(), 4);
+  EXPECT_EQ(p.get_allocator().tag, 1);
+}
+
+TEST(PolymorphicTest, TemplateArgumentDeductionWithDeducedAllocatorAndCopy) {
+  xyz::TaggedAllocator<int> a(1);
+  xyz::polymorphic p(std::allocator_arg, a, Derived(4));
+  xyz::polymorphic pp(std::allocator_arg, 2, p);
+
+  EXPECT_EQ(pp->value(), 4);
+  EXPECT_EQ(pp.get_allocator().tag, 2);
+}
+
+TEST(PolymorphicTest, TemplateArgumentDeductionWithDeducedAllocatorAndMove) {
+  xyz::TaggedAllocator<int> a(1);
+  xyz::polymorphic p(std::allocator_arg, a, Derived(4));
+  xyz::polymorphic pp(std::allocator_arg, 2, std::move(p));
+
+  EXPECT_EQ(pp->value(), 4);
+  EXPECT_EQ(pp.get_allocator().tag, 2);
+}
 }  // namespace
